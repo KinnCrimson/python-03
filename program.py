97db28e406d90f0157f30ffcd4229f13d932df7f
Kevin_Madiou <kevin.madiou.bah@web.de>
2021-04-16 15:15:51 +0200

    fast fettig

--- program.py
+++ program.py
@@ -1,6 +1,11 @@
+import datetime
+
 def main():
     logo()
-    abfrage()
+    jahr, monat, tag = abfrage()
+    datum = geburtsdatum(jahr, monat, tag)
+    restlichetage(datum)
+
 
 
 def logo():
@@ -11,8 +16,22 @@ def logo():
 
 def abfrage():
     print("Wann wurdest du geboren? ")
-    jahr = input("In welchem Jahr [JJJJ]: ")
-    monat = input("In welchem Monat [MM]: ")
-    tag = input("An welchem Tag [TT]: ")
+    jahr = int(input("In welchem Jahr [JJJJ]: "))
+    monat = int(input("In welchem Monat [MM]: "))
+    tag = int(input("An welchem Tag [TT]: "))
+    return jahr, monat, tag
+
+
+def geburtsdatum(jahr, monat, tag):
+    datum = datetime.date(jahr, monat, tag)
+    print(f"Du wurdest also am {datum} geboren.")
+    return datum
+
+
+def restlichetage(datum):
+    heute = datetime.date.today()
+    delta = heute - datum
+    print(f"Du lebst seid {delta.days} Tagen!")
+# WIR MÜSSEN DIE DIFFERENZ VON DEN TAGEN OHNE JAHR HABEN
 
 main()
